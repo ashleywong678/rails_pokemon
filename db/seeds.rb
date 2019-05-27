64a8fea3cf61b2b52dde5329eb444bc6cce41d8c
Ashley <ashleywong678@gmail.com>
2019-05-27 17:32:31 -0400

    created static types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Type.create(name: 'Grass')
+Type.create(name: 'Water')
+Type.create(name: 'Fire')
+Type.create(name: 'Electric')
+Type.create(name: 'Rock')
+Type.create(name: 'Ground')
+Type.create(name: 'Poison')
+Type.create(name: 'Psychic')
+Type.create(name: 'Dark')
+Type.create(name: 'Ghost')
+Type.create(name: 'Fighting')
+Type.create(name: 'Normal')
+Type.create(name: 'Flying')
+Type.create(name: 'Dragon')
+Type.create(name: 'Ice')
+Type.create(name: 'Bug')
