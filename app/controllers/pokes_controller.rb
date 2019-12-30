0ab46acc3086557aff3c799f28d56ba8d9f69ff2
Ashley <ashleywong678@gmail.com>
2019-12-30 11:54:21 -0500

    set show to find pokemon by name

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -5,7 +5,14 @@ class PokesController < ApplicationController
   end
 
   def show
-
+    @poke = Poke.find_by()
   end
 
+  
+  private
+  
+  def find_poke
+    @poke = Poke.find_by(name: params[:name])  
+  end
+  
 end
